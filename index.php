8df5f92e7f178f220009d6837df763ddb91cd55a
unknown <ksiva_god_@bk.ru>
2015-07-23 11:22:10 +0500

    Проверка ЧПУ

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ error_reporting(E_ALL);
 
 require_once __DIR__ . '/autoload/autoload.php';
 
-$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'home';
-$act = isset($_GET['act']) ? $_GET['act'] : 'Index';
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pathParts = explode('/', $path);
+
+$ctrl = !empty($pathParts[1]) ? ucfirst($pathParts[1]) : 'home';
+$act = !empty($pathParts[2]) ? ucfirst($pathParts[2]) : 'Index';
 
 $controllerClassName = 'Aplication\\Controllers\\' . $ctrl;
 
